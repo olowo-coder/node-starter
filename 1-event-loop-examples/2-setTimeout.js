eec974418c9be6c8cab87b58ee85e541d9e6fc99
Olowo Lanre <lanregolowo@gmail.com>
2023-06-24 10:45:04 -0500

    phase two

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,9 @@
+// started operating system process
+console.log('first')
+
+setTimeout(() => {
+  console.log('second')
+}, 0)
+console.log('third')
+
+// completed and exit opreating system process
--- app.js
+++ app.js
@@ -1,7 +1,38 @@
+const { readFile, writeFile } = require('fs').promises
+const util = require('util')
 
-const _ = require('lodash')
+// const readFilePromise = util.promisify(readFile)
+// const writePromise = util.promisify(writeFile)
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
 
-console.log(newItems)
+
+const start = async() => {
+  try{
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    console.log(first, second)
+    await writeFile('./content/result-mind.txt', `THIS IS AWESOME ${first} ${second}`)
+  }catch(error){
+    console.log(error)
+  }
+}
+
+start()
+
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if(err){
+//         console.log(err)
+//         reject(err);
+//       } else{
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
+
+// getText('./content/first.txt')
+// .then(result => console.log(result))
+// .catch(err => console.log(err))
